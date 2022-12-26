e228269c7146beb3aa6e4b6cc15af0793482acf3
YOUSUF <mallick2598456@gmail.com>
2022-12-27 00:16:34 +0530

    final

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,44 +1,49 @@
 package com.driver;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.time.LocalTime;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+class mycomparator implements Comparator<Meeting> {
+    @Override public int compare(Meeting o1, Meeting o2)
+    {
+        return o1.getEndTime().compareTo(o2.getEndTime());
+    }
+}
+
 public class Workspace extends Gmail{
 
-    private ArrayList<Meeting> calendar = new ArrayList<>(); // Stores all the meetings
+    private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
+        // The inboxCapacity is equal to the maximum value an integer can store.
         super(emailId,Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
+
     }
 
     public void addMeeting(Meeting meeting){
-        this.calendar.add(meeting);
-    }
+        //add the meeting to calendar
+        calendar.add(meeting);
 
-    public ArrayList<Meeting> getCalendar() {
-        return calendar;
     }
 
     public int findMaxMeetings(){
-
-        if(calendar.isEmpty()) return 0;
-
-        Collections.sort(calendar, new Sortbyendtime());
-
-        LocalTime curEndTime= calendar.get(0).getEndTime();
-        int maxMeetings= 1;
-
-        for(Meeting meeting : calendar){
-
-            if(meeting.getStartTime().compareTo(curEndTime) > 0){
-                maxMeetings+=1;
-                curEndTime= meeting.getEndTime();
+        // find the maximum number of meetings you can attend
+        // 1. At a particular time, you can be present in at most one meeting
+        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        LocalTime time_limit = LocalTime.ofSecondOfDay(0);
+        int count = 1;
+        mycomparator mc = new mycomparator();
+        Collections.sort(calendar, mc);
+        for(int i =1;i<calendar.size();i++){
+            if(calendar.get(i).getStartTime().compareTo(time_limit) >0){
+                count++;
+                time_limit = calendar.get(i).getEndTime();
             }
         }
-        return maxMeetings;
+        return count;
     }
-}
 }
